a1dc702a4b2e3183af05997c3041fe83568210bc
SwennyS <44819510+SwennyS@users.noreply.github.com>
2019-01-03 19:14:30 +0100

    Add files via upload

---
+++ demo.R
@@ -0,0 +1,11 @@
+##############################Library utiliser, il n'est pas obligatoire mais conseillé de les charger dès le début .... ##############################
+library(dplyr)
+library(stringr)
+library(tidyr)
+library(questionr)
+
+
+##############################donnes geosirene à trouver ici : ##############################
+#http://data.cquest.org/geo_sirene/last/
+
+
